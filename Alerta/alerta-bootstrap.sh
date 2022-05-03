53355d572e5a9c8f8e217705b721ebb0fc6f963f
Anand Kumar <python.marthanda@gmail.com>
2022-05-03 17:48:13 +0530

    update alerta vagrant config

--- Alerta/alerta-bootstrap.sh
+++ Alerta/alerta-bootstrap.sh
@@ -6,45 +6,37 @@ python3 -m venv alerta
 /opt/alerta/bin/pip install --upgrade pip wheel
 
 /opt/alerta/bin/pip install alerta-server alerta pymongo
+# https://github.com/alerta/alerta-contrib/tree/master/plugins/sns
+/opt/alerta/bin/pip install git+https://github.com/alerta/alerta-contrib.git#subdirectory=plugins/sns
 
 cat >>/etc/profile.d/alerta.sh <<EOF
 PATH=$PATH:/opt/alerta/bin
 EOF
 
 cat >/etc/alertad.conf <<EOF
+AUTH_REQUIRED = False
 DEBUG=True
 TESTING=True
 SECRET_KEY='$(< /dev/urandom tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= | head -c 32)'
-PLUGINS=['reject', 'blackout']
+PLUGINS=['reject', 'blackout', 'sns']
 JSON_AS_ASCII=False
 JSON_SORT_KEYS=True
 JSONIFY_PRETTYPRINT_REGULAR=True
-
 DATABASE_URL = 'mongodb://127.0.0.1:27017/?connectTimeoutMS=300000'
 DATABASE_NAME = 'monitoring'
 DATABASE_RAISE_ON_ERROR = False
 MONGO_USERNAME = 'vagrant'
 MONGO_PASSWORD = 'vagrant'
-
-AUTH_REQUIRED = False
 CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
 CORS_ORIGINS = [
     'http://localhost'
 ]
-CORS_SUPPORTS_CREDENTIALS = AUTH_REQUIRED
-
-EMAIL_VERIFICATION = False
-SMTP_HOST = 'smtp.gmail.com'
-SMTP_PORT = 587
-MAIL_FROM = 'devops.marthanda@gmail.com'
-MAIL_TO = 'python.marthanda@gmail.com'
-SMTP_USERNAME = 'devops.marthanda@gmail.com'
-SMTP_PASSWORD = ''
-SMTP_USE_SSL = False
-SKIP_MTA = False
-EMAIL_TYPE = 'text'
-
-ALERTA_DEFAULT_PROFILE=production
+ALLOWED_ENVIRONMENTS = ['Production', 'Development']
+ALERTA_DEFAULT_PROFILE='Production'
+AWS_ACCESS_KEY_ID = $ACCESS_KEY
+AWS_SECRET_ACCESS_KEY = $SECRET_KEY
+AWS_REGION = 'us-east-1'
+AWS_SNS_TOPIC = 'alerta'
 EOF
 
 cat >$HOME/.alerta.conf <<EOF
