a9f228fd79157d7a911627db0295b18a4edb2c05
Anand Kumar <python.marthanda@gmail.com>
2022-05-09 06:34:12 +0530

    Jenkins update firewall

--- Jenkins/Jenkins.sh
+++ Jenkins/Jenkins.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+sudo yum upgrade -y
+sudo yum install -y java-11-openjdk net-tools wget firewalld
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
-sudo yum install -y java-11-openjdk net-tools wget
-sudo yum install jenkins
-sudo systemctl daemon-reload
 
+sudo yum install -y jenkins
+
+sudo systemctl daemon-reload
+sudo systemctl enable firewalld
 sudo systemctl enable jenkins
+
+sudo systemctl start firewalld
 sudo systemctl start jenkins
 
 sleep 1m
