e955f7f3675099da3756107e8454dc29d8bf9766
Anand Kumar <python.marthanda@gmail.com>
2022-05-09 06:18:07 +0530

    Jenkins vagarant upgrade centos7

--- Jenkins/Jenkins.sh
+++ Jenkins/Jenkins.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-echo "Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+sudo yum install -y java-11-openjdk net-tools wget
+sudo yum install jenkins
+sudo systemctl daemon-reload
 
-echo "Updating apt-get"
-sudo apt-get -qq update
-
-echo "Installing default-java"
-sudo apt-get -y install default-jre > /dev/null 2>&1
-sudo apt-get -y install default-jdk > /dev/null 2>&1
-
-echo "Installing git"
-sudo apt-get -y install git > /dev/null 2>&1
-
-echo "Installing git-ftp"
-sudo apt-get -y install git-ftp > /dev/null 2>&1
-
-echo "Installing jenkins"
-sudo apt-get -y install jenkins > /dev/null 2>&1
-sudo service jenkins start
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
 sleep 1m
+
+YOURPORT=8080
+PERM="--permanent"
+SERV="$PERM --service=jenkins"
+
+firewall-cmd $PERM --new-service=jenkins
+firewall-cmd $SERV --set-short="Jenkins ports"
+firewall-cmd $SERV --set-description="Jenkins port exceptions"
+firewall-cmd $SERV --add-port=$YOURPORT/tcp
+firewall-cmd $PERM --add-service=jenkins
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --reload
 
 echo "Installing Jenkins Plugins"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
