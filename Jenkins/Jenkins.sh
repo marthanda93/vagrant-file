22d9cfb26ef87031b0660eb097f8ee55d3f68ab7
Anand Kumar <python.marthanda@gmail.com>
2022-05-09 07:51:59 +0530

    Jenkins, add git

--- Jenkins/Jenkins.sh
+++ Jenkins/Jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum upgrade -y
-sudo yum install -y java-11-openjdk net-tools wget firewalld unzip
+sudo yum install -y java-11-openjdk net-tools wget firewalld unzip git
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
