9d10df391267f35bdd9946a4bdbb11c2605dbcc9
Anand Kumar <python.marthanda@gmail.com>
2022-05-22 08:57:04 +0530

    enable bootstrap.sh

--- grafana_alert-manager/bootstrap.sh
+++ grafana_alert-manager/bootstrap.sh
@@ -7,7 +7,7 @@ sudo systemctl start prometheus
 sudo ufw allow 9090/tcp
 
 # Grafana setup
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add –
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 sudo apt-get -y update
