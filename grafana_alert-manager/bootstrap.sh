4edd60509b10257070baac6c15cfc2856a5c759e
Anand Kumar <python.marthanda@gmail.com>
2022-05-23 22:38:57 +0530

    import dashboard

--- grafana_alert-manager/bootstrap.sh
+++ grafana_alert-manager/bootstrap.sh
@@ -16,38 +16,10 @@ sudo apt-get install grafana -y
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
+sleep 30
 
-# 10180
-# 14513
-# 6287
-# 1860
+if [ $(systemctl is-active grafana-server) != "active" ]; then
+    sleep 30
+fi
 
-
-grafana_cred="admin:admin"
-
-curl -u "$grafana_cred" 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://localhost:9090","access":"proxy","basicAuth":false}'
-# json_object=$(curl -s -k -u "admin:admin" http://192.168.56.10:3000/api/gnet/dashboards/10180 -H 'accept: application/json' | jq .json)
-
-
-# grafana_host="http://localhost:3000"
-# grafana_cred="admin:admin"
-# grafana_datasource="Prometheus"
-# ds=(14513);
-# for d in "${ds[@]}"; do
-#   echo -n "Processing $d: "
-#   j=$(curl -s -k -u "$grafana_cred" $grafana_host/api/gnet/dashboards/$d -H 'accept: application/json' | jq .json)
-#   curl -u "$grafana_cred" $grafana_host/api/dashboards/import \
-#     -H 'accept: application/json, text/plain, */*' \
-#     -H 'content-type: application/json' \
-#     --data-raw "{\"dashboard\":$j,\"overwrite\":true, \
-#         \"inputs\":[{\"name\":\"DS_PROMETHEUS\",\"type\":\"datasource\", \
-#         \"pluginId\":\"prometheus\",\"value\":\"$grafana_datasource\"}]}" \
-#     --compressed \
-#     --insecure; echo ""
-# done
-
-
-# curl -u "admin:admin" 'http://192.168.56.10:3000/api/dashboards/import' \
-#   -H 'accept: application/json, text/plain, */*' \
-#   -H 'content-type: application/json' \
-#   --data-raw $'{"dashboard":{"__inputs":[{"name":"DS_PROMETHEUS",
+curl --user admin:admin 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"Prometheus","type":"prometheus","url":"http://localhost:9090","access":"proxy","basicAuth":false}'
